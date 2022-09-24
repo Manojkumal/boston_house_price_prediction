c326fec7b760b73d846e3b80d1dd8673cfc56efd
Manoj Kumal <manoj.kumal152@gmail.com>
2022-09-24 17:26:38 +0545

    commit for web app

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def predict_api():
     output = model.predict(new_data)
     return jsonify(output[0])
 
+@app.route("/predict",methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    output = model.predict(final_input)[0]
+    return render_template('home.html',prediction_text=f"The house price prediction is {output}")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
